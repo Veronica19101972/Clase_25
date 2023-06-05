8eda538e40462a32f2684b78832b3d75a741bc64
Verónica Montaña <veronicamontana@gmail.com>
2023-06-05 20:47:03 -0300

    commit

---
+++ Coderhouse_40445/project/apps/home/urls.py
@@ -0,0 +1,14 @@
+# * Importar LogoutView
+from django.contrib.auth.views import LogoutView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", LogoutView.as_view(template_name="home/logout.html"), name="logout"),
+    path("register/", views.register, name="register"),
+    path("about/", views.about, name="about"),
+] + staticfiles_urlpatterns()
